fa3ae754091871fc1377d653b221cf5f6e5c53d5
DenisProh <denisprohlive@gmail.com>
2025-07-07 18:29:58 +0300

    project was created

---
+++ main.py
@@ -0,0 +1,19 @@
+#подключение библиотеки
+from pygame import *
+
+#создание окна игры
+win = display.set_mode((700,500))
+display.set_caption('PingPong')
+
+
+game = True
+
+clock = time.Clock()
+FPS=60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
